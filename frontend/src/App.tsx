d6229b1caa96f128eb7c9e7ac3ed1d9190523507
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-22 23:45:06 +0530

    404 and some work in Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import Login from "./components/Login"
 import Signup from "./components/Signup"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import 'remixicon/fonts/remixicon.css'
+import Layout from "./components/app/Layout"
+
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +16,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/app" element={<Layout />} />
+        <Route path="*" element={<div className="flex items-center justify-center text-5xl h-screen font-bold">Kya Chahiye bhai 😇</div>} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ frontend/src/components/app/Layout.tsx
@@ -0,0 +1,16 @@
+const Layout = () => {
+  return (
+    <div className="min-h-screen">
+        <aside className="w-64 h-screen bg-red-500 fixed top-0 left-0 text-white p-8 overflow-auto">
+          <div className="p-4">
+            <h1 className="text-xl font-bold">Besties</h1>
+          </div>
+        </aside>
+        <main className="ml-64 p-4">
+        </main>
+        <aside className=""></aside>
+    </div>
+  )
+}
+
+export default Layout
