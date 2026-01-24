89007bd8af34085e91ba6a3a07882ac5432adce6
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-24 23:53:36 +0530

    shared components Avatar

---
+++ frontend/src/components/shared/Avatar.tsx
@@ -0,0 +1,35 @@
+import type { FC, ReactNode } from "react"
+
+interface AvatarInterface {
+    title?: string
+    subtitle?: ReactNode
+    image?: string
+    titleColor?: string
+    subtitleColor?: string
+    size?: "lg" | "md"
+}
+
+const Avatar: FC<AvatarInterface> = ({size="lg", title, subtitle="Subtitle missing", image, titleColor="#000000", subtitleColor="#f5f5f5"}) => {
+  return (
+    <div className="flex gap-3 items-center">
+        {
+            image &&
+            <img 
+                src={image} 
+                className={`${size === "lg" ? "w-12 h-12" : "w-8 h-8"} rounded-full object-cover`}
+            />
+        }
+        {
+            (title && subtitle) &&
+            <div className="flex flex-col">
+                <h1 className={`${size === "lg" ? "text-lg/6" : "text-base"} font-medium`} style={{color: titleColor}}>{title}</h1>
+                <div style={{color: subtitleColor}}>
+                    {subtitle}
+                </div>
+            </div>
+        }
+    </div>
+  )
+}
+
+export default Avatar
