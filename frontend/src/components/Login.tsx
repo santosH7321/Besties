2a8eb210be2c2740ec1d2e6af02f2aff2330d895
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-30 01:35:01 +0530

    login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,7 +1,45 @@
+import { Link } from "react-router-dom"
+import Button from "./shared/Button"
+import Card from "./shared/Card"
+import Input from "./shared/Input"
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center text-5xl h-screen font-bold">Login</div>
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-6/12 animate__animated animate__fadeIn">
+        <Card noPadding>
+          <div className="grid grid-cols-2">
+            <div className="p-8 space-y-6">
+              <div>
+                <h1 className="text-xl font-bold text-black">SIGN IN</h1>
+                <p className="text-gray-500">Start your first chat now !</p>
+              </div>
+              <form className="space-y-6">
+                <Input 
+                  name="email"
+                  placeholder="Email id"
+                />
+
+                <Input 
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+
+                <Button type="danger" icon="arrow-right-up-line">Sign in</Button>
+              </form>
+              <div className="flex gap-2">
+                <p>Don`t` have an account ?</p>
+                <Link to="/signup" className="text-green-400 font-medium hover:underline">Sign up</Link>
+              </div>
+            </div>
+            <div className="overflow-hidden h-125 bg-linear-to-t from-sky-500 to-indigo-500 flex items-center justify-center rounded-r-xl">
+                <img src="/images/login.svg" alt="auth" className="w-[70%] animate__animated animate__slideInUp animate__faster" />
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
   )
 }
 
