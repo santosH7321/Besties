fa72e6c587382ecfe76b785051b9b92a55b68cef
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-30 22:54:11 +0530

    basic server configuration setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,19 @@
-console.log("Hello jee")
-console.log("TypeScript setup ho gyi hai")
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+mongoose.connect(process.env.DB!)
+
+import express from "express";
+import cors from "cors";
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
