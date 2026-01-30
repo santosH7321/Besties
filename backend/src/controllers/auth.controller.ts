7d6c407906d17903e0a4d91cb0e7b50d7d41939d
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-31 00:15:21 +0530

    login controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from "express"
 import AuthModel from "../models/auth.model";
-
+import bcrypt from "bcrypt";
 
 export const signup = async (req: Request, res: Response) => {
     try {
         await AuthModel.create(req.body)
-        res.status(200).json({message: "Signup Success ✅"});
+        res.json({message: "Signup Success ✅"});
     } catch (err: any) {
         res.status(500).json({message: err.message});
     }
 }
 
-export const login = (req: Request, res: Response) => {
-    res.send("Login Success ✅");
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    const user = await AuthModel.findOne({email});
+    if(!user){
+        throw new Error("User not found, please try to signup first")
+    }
+
+    const isLogin = await bcrypt.compare(password, user.password);
+    if(!isLogin)
+        throw new Error("Invalid Credentials email and password incorrect");
+
+    res.json({message: "Login Success ✅"});
 }
 
 export const forgotPassword = (req: Request, res: Response) => {
