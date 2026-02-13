c7c315aa69b842bab1d940e02d5f74bbebe30fab
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-02-13 23:29:19 +0530

    chat-Socket backend

---
+++ backend/src/socket/chat.socket.ts
@@ -0,0 +1,14 @@
+import { Server } from "socket.io"
+
+const ChatSocket = (io: Server)=>{
+    io.on("connection", (socket)=>{
+        socket.on("message", (payload)=>{
+            io.to(payload.to).emit("message", {
+                from: payload.from,
+                message: payload.message
+            })
+        })
+    })
+}
+
+export default ChatSocket
