c2dec7eea9b32f7f5579cdde8bd447e6989bcf2b
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-02-16 23:49:23 +0530

    Attachement file

--- backend/src/socket/chat.socket.ts
+++ backend/src/socket/chat.socket.ts
@@ -1,5 +1,6 @@
 import { Server } from "socket.io"
 import { createChat } from "../controllers/chat.controller"
+import { downloadObject } from "../utils/s3"
 
 const ChatSocket = (io: Server)=>{
     io.on("connection", (socket)=>{
@@ -11,6 +12,21 @@ const ChatSocket = (io: Server)=>{
             io.to(payload.to).emit("message", {
                 from: payload.from,
                 message: payload.message
+            })
+        })
+        socket.on("attachment", async (payload)=>{
+            createChat({
+                ...payload,
+                from: payload.from.id
+            })
+
+            io.to(payload.to).emit("attachment", {
+                from: payload.from,
+                message: payload.message,
+                file: {
+                    path: await downloadObject(payload.file.path),
+                    type: payload.file.type
+                }
             })
         })
     })
