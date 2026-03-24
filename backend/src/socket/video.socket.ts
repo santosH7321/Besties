495241a1920fc52eea732043f584160aee8e4543
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-24 22:13:20 +0530

    add video socket candidate anser end

--- backend/src/socket/video.socket.ts
+++ backend/src/socket/video.socket.ts
@@ -1,12 +1,23 @@
 import { Server } from "socket.io"
 
 const VideoSocket = (io: Server)=>{
-    io.on("connection", (socket)=>{
-        
+    io.on("connection", (socket)=>{ 
+
         socket.on("offer", ({offer, to})=>{
             io.to(to).emit("offer", {offer, from: socket.id})
         })
-        
+
+        socket.on("candidate", ({candidate, to})=>{
+            io.to(to).emit("candidate", {candidate, from: socket.id})
+        })
+
+        socket.on("answer", ({answer, to})=>{
+            io.to(to).emit("answer", {answer, from: socket.id})
+        })
+
+        socket.on("end", ({to})=>{
+            io.to(to).emit("end", {from: socket.id})
+        })
     })
 }
 
