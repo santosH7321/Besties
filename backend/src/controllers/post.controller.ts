dc1aedf2971fc936cff38628266a499689322b64
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-25 15:17:48 +0530

    add fetchPost updatePost and deletePost controller

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -14,3 +14,43 @@ export const createPost = async (req: SessionInterface, res: Response)=>{
         CatchError(err, res, "Failed to create post")
     }
 }
+
+export const fetchPosts = async (req: SessionInterface, res: Response)=>{
+    try {
+        const userId = req.session?.id
+        const posts = await PostModel.find({user: userId}).sort({createdAt: -1})
+        res.json(posts)
+    }
+    catch(err)
+    {
+        CatchError(err, res, "Failed to fetch post")
+    }
+}
+
+export const updatePost = async (req: SessionInterface, res: Response)=>{
+    try {
+        const post = await PostModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        if(!post)
+            throw TryError("Failed to find post for update")
+
+        res.json(post)
+    }
+    catch(err)
+    {
+        CatchError(err, res, "Failed to update post")
+    }
+}
+
+export const deletePost = async (req: SessionInterface, res: Response)=>{
+    try {
+        const post = await PostModel.findByIdAndDelete(req.params.id)
+        if(!post)
+            throw TryError("Failed to find post for update")
+
+        res.json(post)
+    }
+    catch(err)
+    {
+        CatchError(err, res, "Failed to delete post")
+    }
+}
--- backend/src/router/post.router.ts
+++ backend/src/router/post.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller";
+import { createPost, deletePost, fetchPosts, updatePost } from "../controllers/post.controller";
 const PostRouter = Router()
 
 PostRouter.post('/', createPost)
-
+PostRouter.get('/', fetchPosts)
+PostRouter.put('/:id', updatePost)
+PostRouter.delete('/:id', deletePost)
 
 export default PostRouter
