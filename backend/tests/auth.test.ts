985d2ab19bef42867b4c5dbda0e2ec925b2bbdb7
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-04-10 02:33:40 +0530

    add login test

--- backend/tests/auth.test.ts
+++ backend/tests/auth.test.ts
@@ -1,7 +1,26 @@
 import request from "supertest";
 import app from "../src/app";
+import AuthModel from "../src/models/auth.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+(AuthModel as any).findOne = jest.fn().mockResolvedValue({
+  _id: "123",
+  fullname: "Test User",
+  email: "test@test.com",
+  password: "hashedPassword",
+  mobile: "1234567890",
+  image: "img.png"
+});
+
+(bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+(jwt.sign as jest.Mock).mockReturnValue("mockAccessToken");
 
 jest.mock("../src/models/auth.model");
+jest.mock("../src/models/auth.model");
+jest.mock("bcrypt");
+jest.mock("jsonwebtoken");
 
 
 describe("Auth Routes", () => {
@@ -17,4 +36,19 @@ describe("Auth Routes", () => {
 
     expect(res.body.message).toBe("Signup Success ✅");
   });
+
+  it("should login user successfully", async () => {
+      const res = await request(app)
+        .post("/auth/login")
+        .send({
+          email: "test@test.com",
+          password: "123456"
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe("Login Success 🎉");
+
+      expect(res.headers["set-cookie"]).toBeDefined();
+    });
 });
+
