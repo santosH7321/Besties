67eeabcb559b143475738e8a679baf11b489edb3
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-04-10 17:12:47 +0530

    url bug fixing

--- frontend/src/lib/HttpInterceptor.ts
+++ frontend/src/lib/HttpInterceptor.ts
@@ -2,7 +2,7 @@ const env = import.meta.env
 import axios from "axios";
 
 const HttpInterceptor = axios.create({
-    baseURL: env.VITE_SERVER,
+    baseURL: env.VITE_API_URL,
     withCredentials: true
 })
 
